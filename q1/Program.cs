8bcde2859e5163c352aa0e21885d7105e1680c52
FLAVIO <flavio.sousa.dc@gmail.com>
2025-04-14 20:43:11 -0300

    primeiro

---
+++ q1/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string palavra = "banana";
+        char[] resposta = new string('_', palavra.Length).ToCharArray();
+        List<char> letrasErradas = new List<char>();
+        int tentativas = 6;
+
+        while (tentativas > 0 && new string(resposta) != palavra)
+        {
+            Console.Clear();
+            Console.WriteLine("Palavra: " + string.Join(" ", resposta));
+            Console.WriteLine("Tentativas restantes: " + tentativas);
+            Console.WriteLine("Letras erradas: " + string.Join(", ", letrasErradas));
+            Console.Write("Digite uma letra: ");
+            char letra = char.ToLower(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            if (palavra.Contains(letra))
+            {
+                for (int i = 0; i < palavra.Length; i++)
+                    if (palavra[i] == letra)
+                        resposta[i] = letra;
+            }
+            else
+            {
+                if (!letrasErradas.Contains(letra))
+                {
+                    letrasErradas.Add(letra);
+                    tentativas--;
+                }
+            }
+        }
+
+        if (new string(resposta) == palavra)
+            Console.WriteLine("Parabéns! Você acertou: " + palavra);
+        else
+            Console.WriteLine("Você perdeu! A palavra era: " + palavra);
+    }
+}
