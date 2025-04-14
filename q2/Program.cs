d8ef76280debde26ae7fee4c10a4ff153d2e69a8
FLAVIO <flavio.sousa.dc@gmail.com>
2025-04-14 20:58:01 -0300

    Prim

--- q2/Program.cs
+++ q2/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class JogoAdivinhacao
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int numeroSecreto = random.Next(1, 1001); // número entre 1 e 1000
+        int tentativa;
+
+        Console.WriteLine("Tente adivinhar o número entre 1 e 1000.");
+
+        do
+        {
+            Console.Write("\nDigite sua tentativa: ");
+            tentativa = int.Parse(Console.ReadLine());
+
+            int diferenca = Math.Abs(numeroSecreto - tentativa);
+
+            if (tentativa == numeroSecreto)
+            {
+                Console.WriteLine("Parabéns! Você acertou!");
+                break;
+            }
+            else
+            {
+                if (diferenca > 100)
+                    Console.WriteLine("Muito distante!");
+                else if (diferenca > 50)
+                    Console.WriteLine("Distante!");
+                else
+                    Console.WriteLine("Perto!");
+
+                Console.WriteLine(tentativa < numeroSecreto ? "O número é MAIOR." : "O número é MENOR.");
+            }
+
+        } while (true);
+    }
+}
