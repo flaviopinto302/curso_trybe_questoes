c4ccd86f3c50be17d5948044984199f5e04f4186
FLAVIO <flavio.sousa.dc@gmail.com>
2025-04-14 21:02:59 -0300

    todas

--- q1/obj/Debug/net9.0/q1.AssemblyInfo.cs
+++ q1/obj/Debug/net9.0/q1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("q1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8ef76280debde26ae7fee4c10a4ff153d2e69a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("q1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("q1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
